4c088d4350a0498d7ce3cbacb2323691620f7b95
mohamdhashim <mohamdhashiem@gmail.com>
2022-01-12 13:48:23 +0200

    "Refactorying Old Code + add Email Vaildation"

--- resources/js/app.js
+++ resources/js/app.js
@@ -22,7 +22,10 @@
  // const files = require.context('./', true, /\.vue$/i)
  // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
  
+ //Remeber Registered Value Must be in KABAB Case to Work
  Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+ Vue.component('input-label', require('./components/Input.vue').default);
+
  
  /**
   * Next, we will create a fresh Vue application instance and attach it to
--- resources/js/pages/NewAddress.vue
+++ resources/js/pages/NewAddress.vue
@@ -1,66 +1,102 @@
 <template>
-  <div class="home">
+    <div>
+        <form @submit.prevent="handleSubmit">
+            <input-label label_text="Building" :method="update_building" />
+            <input-label label_text="Apartment" :method="update_apartment" />
+            <input-label label_text="Floor" :method="update_floor" />
+            <input-label label_text="Street" :method="update_street" />
 
-    <form @submit.prevent= "handleSubmit" >
+            <div style="color: red" v-if="validateEmail()">
+                *E-mail not Valid
+            </div>
+            <input-label label_text="Email" :method="update_email" />
 
-      <inputlabel label_text="Building" v-model="building"/>
-      <inputlabel label_text="Apartment" v-model="apartment"/>
-      <inputlabel label_text="Floor" v-model="floor"/>
-      <inputlabel label_text="Street" v-model="street"/>
-      <inputlabel label_text="Email" v-model="email"/>
-      <inputlabel label_text="Area name" v-model="area_name"/>
-      <div class = "submit">
-        <button>POST Data</button>
-      </div>
-    </form>
-    
-  </div>
+            <input-label label_text="Area name" :method="update_area_name" />
+            <div class="submit">
+                <button>POST Data</button>
+            </div>
+        </form>
+    </div>
 </template>
 
 <script>
-import inputlabel from '../components/Input.vue'
-    
-  export default {
-  components: { inputlabel },
-    name: 'NewAddress',
-    data(){
-      return {
-        apartment: '',
-        building: '',
-        floor: '',
-        street: '',
-        area_name:'',
-        email: '',
-        message:''
-      }
-    },methods: {
-      handleSubmit(){
-        var address = {
-            floor: this.floor,
-            building: this.building,
-            street: this.street,
-            apartment: this.apartment,
-            area: this.area_name,
-            email: this.email
-        }
-        
-        axios.post('http://localhost/api/new_address',address)
-          .then(data => {console.log('Message', data);})
-          .catch((error) => {
-          console.error('Error:', error);
-          });
-      }
-    }
-  }
+import axios from "axios";
+
+export default {
+    name: "NewAddress",
+    data() {
+        return {
+            apartment: "",
+            building: "",
+            floor: "",
+            street: "",
+            area_name: "",
+            email: "",
+            message: "",
+        };
+    },
+
+    methods: {
+        //update methods used to retreive data from child components
+        update_apartment(valueFromChild) {
+            this.apartment = valueFromChild;
+        },
+        update_building(valueFromChild) {
+            this.building = valueFromChild;
+        },
+        update_floor(valueFromChild) {
+            this.floor = valueFromChild;
+        },
+        update_street(valueFromChild) {
+            this.street = valueFromChild;
+        },
+        update_area_name(valueFromChild) {
+            this.area_name = valueFromChild;
+        },
+        update_email(valueFromChild) {
+            this.email = valueFromChild;
+        },
+
+        handleSubmit() {
+            var address = {
+                floor: this.floor,
+                building: this.building,
+                street: this.street,
+                apartment: this.apartment,
+                area: this.area_name,
+                email: this.email,
+            };
+
+            axios
+                .post("http://localhost/api/new_address", address)
+                .then((data) => {
+                    console.log("Message", data);
+                })
+                .catch((error) => {
+                    console.error("Error:", error);
+                });
+        },
+
+        validateEmail() {
+            if (
+                /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.email)
+            ) {
+                return false;
+            } else {
+                return true;
+            }
+        },
+    },
+};
 </script>
 <style scoped>
-  input{
-    margin-bottom: 15px ;
+input-label {
+    margin-bottom: 15px;
     padding: 10px 100px;
-    text-align:center;
+    text-align: center;
     text-anchor: none;
-  }
-  form{
+}
+form {
     display: inline-block;
-  }
+}
 </style>
