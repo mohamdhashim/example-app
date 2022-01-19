81186bf8e2992d36b7ddaea3cfe80b5c96e03643
mohamdhashim <mohamdhashiem@gmail.com>
2022-01-19 17:18:08 +0200

    dependencies & configs

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{any}', function () {
-    return view('layouts.vue');
-})->where('any', '.*');
+// Route::get('/{any}', function () {
+//     return view('layouts.vue');
+// })->where('any', '.*');
+
+Route::get('/',App\Http\Livewire\Welcome::class);
+Route::get('/createaddress',App\Http\Livewire\NewAddress::class);
+
+
+Route::get("/addresses",App\Http\Livewire\AddressTable::class);
